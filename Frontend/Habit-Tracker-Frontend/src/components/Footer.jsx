32db6ad76559da781b7017a046220a65e8afeefd
marius <you@example.com>
2023-09-13 12:01:10 +0200

    Created Footer with dynamic Year

--- Frontend/Habit-Tracker-Frontend/src/components/Footer.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/Footer.jsx
@@ -1,5 +1,10 @@
+
 function Footer() {
-    return null;
+  return (
+    <>
+        <div class="card-footer text-muted">(c) Dzafic, Paredes, Schroeder {new Date().getFullYear()}</div>
+    </>
+  );
 }
 
 export default Footer;
