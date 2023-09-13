83c694b4ff94b5b2e14c4f993974968dfe21b17d
maksi <maksi9900@outlook.com>
2023-09-13 21:05:15 +0200

    Preparing Single-Page logic and adding directly Navbar Component

--- Frontend/Habit-Tracker-Frontend/src/components/NavBar.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/NavBar.jsx
@@ -1,18 +1,30 @@
-import "../components/NavBarStyles.css"
+//import "../components/NavBarStyles.css"
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import {Navbar, Nav, Container} from "react-bootstrap"
 
-export default function Navbar() {
+
+export default function NavBar() {
     return (
-
-        <nav className="NavbarItems">
-            <Link to="/" className="navbar-logo">
-                Habit.Tracker
-            </Link>
-            <ul className="nav-menu">
-                <CustomLink className="nav-links" to="/logout">Log Out</CustomLink>
-                <CustomLink className="nav-links" to="/info">Info</CustomLink>
-            </ul>
-        </nav>
+<div class="container">
+<nav class="navbar bg-body-tertiary fixed-top navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <Link to ="/" class="navbar-brand">Habit-Tracker</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ml-auto" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <CustomLink class="nav-link" aria-current="page" to="/info">Info</CustomLink>
+        </li>
+        <li class="nav-item">
+          <CustomLink class="nav-link" to="/logout">Log Out</CustomLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
     )
 }
 
@@ -28,3 +40,30 @@ function CustomLink({ to, children, ...props }) {
         </li>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//COULD BE HELPFUL, hier bitte stehen lassen. Mahir
+        // <nav className="NavbarItems">
+        //     <Link to="/" className="navbar-logo">
+        //         Habit.Tracker
+        //     </Link>
+        //     <ul className="nav-menu">
+        //         <CustomLink className="nav-links" to="/logout">Log Out</CustomLink>
+        //         <CustomLink className="nav-links" to="/info">Info</CustomLink>
+        //     </ul>
+        //     </nav>
