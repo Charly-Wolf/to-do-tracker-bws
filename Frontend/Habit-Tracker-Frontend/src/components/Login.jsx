e4aafefdd6d84bb0331e8d0bfda0fe6d8e4efba1
Carlos <cardan1990@hotmail.com>
2023-09-18 09:37:38 +0200

    Created basic Login component

--- Frontend/Habit-Tracker-Frontend/src/components/Login.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/Login.jsx
@@ -1,7 +1,64 @@
+import { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="container h-100">
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="row h-100 justify-content-center align-items-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="text-center">Login</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <br />
+                <button type="submit" className="btn btn-primary btn-block">
+                  Login
+                </button>
+              </form>
+              <p className="mt-3 text-center">
+                Do you already have an account? <a href="/register">Register</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
