486d8b7c202bfba56648900c5b1c8f21a0d3d01c
Charly <cardan1990@hotmail.com>
2023-09-21 22:35:31 +0200

    Added Test Background for Login and Register

--- Frontend/Habit-Tracker-Frontend/src/components/Login.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
+import "../assets/css/loginStyles.css"
 
 const client = axios.create({
   baseURL: "http://127.0.0.1:5000/api/login", // Connection with the Backend
@@ -40,59 +41,51 @@ function Login({ setIsLoggedIn, setIsAdmin }) {
   };
 
   return (
-    <div className="container h-100">
-      <br />
-      <br />
-      <div className="row h-100 justify-content-center align-items-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="text-center">Login</h2>
-              <br />
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                    required
-                  />
-                </div>
-                <br />
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                    required
-                  />
-                </div>
-                <br />
-                <button type="submit" className="btn btn-primary btn-block">
-                  Login
-                </button>
-              </form>
-              {/* Display the error message with Bootstrap danger style */}
-              {error && <div className="alert alert-danger mt-3">{error}</div>}
-
-              {/* TODO */}
-              {/* <p className="mt-3 text-center">
-                Forgot your <Link to="#">Password</Link>?
-              </p> */}
-
-              <p className="mt-3 text-center">
-                You don&#39;t have an account yet?{" "}
-                <Link to="/register">Register</Link>
-              </p>
-            </div>
+    <div className="main-body">
+      <div className="card main-card">
+        <h2 className="text-center card-title">Login</h2>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
           </div>
-        </div>
+          <br />
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <br />
+          <button type="submit" className="btn btn-primary btn-block">
+            Login
+          </button>
+        </form>
+        {/* Display the error message with Bootstrap danger style */}
+        {error && <div className="alert alert-danger mt-3">{error}</div>}
+
+        {/* TODO */}
+        {/* <p className="mt-3 text-center">
+          Forgot your <Link to="#">Password</Link>?
+        </p> */}
+
+        <p className="mt-3 text-center">
+          You don&#39;t have an account yet?{" "}
+          <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
