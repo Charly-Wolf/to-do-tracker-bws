6f1425c1c4fdda4d41072eca38fab8884cba5e8d
Charly <cardan1990@hotmail.com>
2023-09-22 12:56:50 +0200

    Fixed errors

--- Frontend/Habit-Tracker-Frontend/src/components/HabitBox.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/HabitBox.jsx
@@ -1,15 +1,16 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+// import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import PropTypes from "prop-types";
 
-let habitTitle = "Test Habit";
-let habitDescription = "This is just an Description for this test habit. :)  I wonder whats happening, when there is an overflow. bbodso ifjdas ifdsaüf jadsfsadjfj"
+// let habitTitle = "Test Habit";
+// let habitDescription = "This is just an Description for this test habit. :)  I wonder whats happening, when there is an overflow. bbodso ifjdas ifdsaüf jadsfsadjfj"
 
-export default function HabitBox(props) {
+export default function HabitBox({ title }) {
   return (
     <>
       <div className="card text-bg-dark mb-3 mx-auto" style={{ maxWidth: "18rem" }}>
         <div className="card-body row">
           <div className="col-9 d-flex flex-column justify-content-center" style={{ maxHeight: "20vh" }}>
-            <h5 className="card-title" id="habitTitle">{props.title}</h5>
+            <h5 className="card-title" id="habitTitle">{title}</h5>
             {/*<p className="card-text overflow-y-scroll" id="habitDescription">
               {props.description}
   </p>*/}
@@ -17,8 +18,8 @@ export default function HabitBox(props) {
           <div className="col-2">
             <form>
               <div className="row">
-              <input type="checkbox" className="btn-check" id="btn-check-outlined" autocomplete="off"/>
-              <label className="btn btn-outline-success my-1" for="btn-check-outlined"><i className="bi bi-check2"></i></label>
+              <input type="checkbox" className="btn-check" id="btn-check-outlined" autoComplete="off"/>
+              <label className="btn btn-outline-success my-1" htmlFor="btn-check-outlined"><i className="bi bi-check2"></i></label>
               </div>
               <div className="row">
                 <a href="#" className="btn btn-outline-primary bi bi-pencil my-1"></a>
@@ -36,16 +37,6 @@ export default function HabitBox(props) {
   );
 }
 
-
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
-}
+HabitBox.propTypes = {
+  title: PropTypes.string.isRequired,
+};
--- Frontend/Habit-Tracker-Frontend/src/components/HabitList.jsx
+++ Frontend/Habit-Tracker-Frontend/src/components/HabitList.jsx
@@ -1,5 +1,5 @@
 import HabitBox from "../components/HabitBox"; //Muss noch für jedes Habit angezeigt werden.
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -12,13 +12,13 @@ function HabitList() {
   const [habitsError, setHabitsError] = useState("");
   
     // Teilt die habits in Gruppen von drei auf
-    const test = "test";
+    // const test = "test";
     let groupedHabits = [];
     for (let i = 0; i < habits.length; i += 3) {
       groupedHabits.push(habits.slice(i, i + 3));
     }
-    console.log(test)
-    console.log(groupedHabits)
+    // console.log(test)
+    // console.log(groupedHabits)
 
   useEffect(() => {
     const fetchHabits = async () => {
@@ -38,7 +38,7 @@ function HabitList() {
       <div className="card-header">
         <ul className="nav nav-pills card-header-pills nav-light bg-white">
           <li className="nav-item ms-auto">
-            <CustomLink className="nav-link" to="/">
+            <Link className="nav-link active" to="/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="22"
@@ -50,35 +50,36 @@ function HabitList() {
                 <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
                 <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"></path>
               </svg>
-            </CustomLink>
+            </Link>
           </li>
           <li className="nav-item">
-            <CustomLink
-              className="nav-link active"
+            <Link
+              className="nav-link"
               aria-current="true"
               to="/stats"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-                fill="currentColor"
-                className="bi bi-bar-chart-line-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2z"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                    width="16" 
+                    height="16" 
+                    fill="currentColor" 
+                    className="bi bi-bar-chart" 
+                    viewBox="0 0 16 16">
+                <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"/>
               </svg>
-            </CustomLink>
+            </Link>
           </li>
           <li className="nav-item">
-            <CustomLink className="nav-link" to="/stats">
+            <Link className="nav-link" to="/stats">
               <i className="bi bi-plus-circle-fill"></i>
-            </CustomLink>
+            </Link>
           </li>
         </ul>
       </div>
 
-      <div className="card-body">
+      {habitsError ? (
+        <div className="alert alert-danger mt-3">{habitsError}</div>
+      ) : (
+        <div className="card-body">
         <div className="container mx-auto border p-5">
           <h5 className="card-title">Habit Liste</h5>
           <p className="card-text">
@@ -91,34 +92,23 @@ function HabitList() {
             {groupedHabits.map((group, groupIndex) => (
               <div className="row" key={groupIndex}>
                 {group.map((habit, index) => (
-                  <div className="col" key={index} sm={4}>
+                  // <div className="col" key={index} sm={4}>
+                  <div className="col" key={index}>
                     <HabitBox
                       title={
                         habit.name
                       } /*description={habit.description} checked={habit.status}*/
                     />
                   </div>
-                ))}
-              </div>
-            ))}
+                  ))}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
 
 export default HabitList;
-
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
-}
